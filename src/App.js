4ee3ed55ee8aa7eb211868db80a0ab1efdd774ad
Eduardo2020 <dazaeduardo79@gmail.com>
2024-06-02 19:29:31 -0500

    funciona cerrar secion

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import Login from './Login';
 import Register from './Register';
 import Dashboard from './Dashboard';
+import Perfil from './Perfil'; // Importa el componente Perfil
 import './App.css';
 
 function App() {
-  const [view, setView] = useState('login'); // 'login', 'register', 'dashboard'
+  const [view, setView] = useState('login'); // 'login', 'register', 'dashboard', 'perfil'
 
   const toggleView = (newView) => {
     setView(newView);
@@ -16,6 +17,7 @@ function App() {
       {view === 'login' && <Login toggleView={toggleView} />}
       {view === 'register' && <Register toggleView={toggleView} />}
       {view === 'dashboard' && <Dashboard />}
+      {view === 'perfil' && <Perfil />} {/* Agrega el renderizado del Perfil */}
     </div>
   );
 }
