78164bea08f3f7aeb5a2331d11398e145524cd20
Jessica Pimentel <tester2@greenhorizon.com.co>
2024-06-02 19:59:00 -0500

    inicio con google

--- src/Iniciogoogle.js
+++ src/Iniciogoogle.js
@@ -4,7 +4,7 @@ import Loginsesion from './Loginsesion';
 
 export default function Iniciogoogle() {
   return (
-    <GoogleOAuthProvider clientId="567858506235-la5dh2n100tn8hovkis4vtbd1fikv0td.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="567858506235-sd9fvbkheo3rnggdfpmnfjp63t6rgej3.apps.googleusercontent.com">
       <main>
         <div>
           <h1>Google</h1>
